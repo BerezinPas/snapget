b145f327a0e803b7a814b62d2b78647b7647816b
Berezin <pas.1999@mail.ru>
2025-08-09 23:30:06 +0700

    delete request consults

--- inc/fieldGroups/postComponents.php
+++ inc/fieldGroups/postComponents.php
@@ -11,13 +11,13 @@ add_action('Flynt/afterRegisterComponents', function () {
         'orderby' => 'title',
         'order' => 'ASC'
     ]);
-    
+
     $form_choices = [];
     foreach ($cf7_forms as $form) {
         $form_choices[$form->ID] = $form->post_title;
     }
 
-    
+
     ACFComposer::registerFieldGroup([
         'name' => 'postComponents',
         'title' => __('Post Components', 'flynt'),
@@ -68,56 +68,56 @@ add_action('Flynt/afterRegisterComponents', function () {
                 'bidirectional_target' => [
                 ],
             ],
-            [
-                'label' => 'Добавить файлы для скачивания',
-                'name' => 'download_files',
-                'type' => 'repeater',
-                'button_label' => 'Добавить файл',
-                'sub_fields' => [
-                    [
-                        'label' => 'Название файла',
-                        'name' => 'file_name',
-                        'type' => 'text',
-                        'instructions' => 'Можете переименовать файл, если не надо оставьте поле пустым',
-                        'required' => 0
-                    ],
-                    [
-                        'label' => 'Файл',
-                        'name' => 'file',
-                        'type' => 'file',
-                        'return_format' => 'array',
-                        'library' => 'all',
-                        'required' => 1
-                    ]
-                ]
-            ],
+            // [
+            //     'label' => 'Добавить файлы для скачивания',
+            //     'name' => 'download_files',
+            //     'type' => 'repeater',
+            //     'button_label' => 'Добавить файл',
+            //     'sub_fields' => [
+            //         [
+            //             'label' => 'Название файла',
+            //             'name' => 'file_name',
+            //             'type' => 'text',
+            //             'instructions' => 'Можете переименовать файл, если не надо оставьте поле пустым',
+            //             'required' => 0
+            //         ],
+            //         [
+            //             'label' => 'Файл',
+            //             'name' => 'file',
+            //             'type' => 'file',
+            //             'return_format' => 'array',
+            //             'library' => 'all',
+            //             'required' => 1
+            //         ]
+            //     ]
+            // ],
             // форма обратной связи
-             [
-                'label' => 'Показать форму обратной связи"',
-                'name' => 'enable_request_consultation',
-                'type' => 'true_false',
-                'default_value' => 0,
-                'ui' => 1,
-                'wrapper' => [
-                    'width' => '50%'
-                ],
-                'instructions' => 'Отображает форму справа от контента'
-            ],
-            [
-                'label' => 'Выберите форму',
-                'name' => 'cf7_form_id',
-                'type' => 'select',
-                'choices' => $form_choices,
-                'conditional_logic' => [
-                    [
-                        [
-                            'field' => 'enable_request_consultation',
-                            'operator' => '==',
-                            'value' => '1'
-                        ]
-                    ]
-                ]
-            ],
+            //  [
+            //     'label' => 'Показать форму обратной связи"',
+            //     'name' => 'enable_request_consultation',
+            //     'type' => 'true_false',
+            //     'default_value' => 0,
+            //     'ui' => 1,
+            //     'wrapper' => [
+            //         'width' => '50%'
+            //     ],
+            //     'instructions' => 'Отображает форму справа от контента'
+            // ],
+            // [
+            //     'label' => 'Выберите форму',
+            //     'name' => 'cf7_form_id',
+            //     'type' => 'select',
+            //     'choices' => $form_choices,
+            //     'conditional_logic' => [
+            //         [
+            //             [
+            //                 'field' => 'enable_request_consultation',
+            //                 'operator' => '==',
+            //                 'value' => '1'
+            //             ]
+            //         ]
+            //     ]
+            // ],
             [
                 'label' => 'Выберите стикер',
                 'name' => 'sticker_type',
@@ -128,9 +128,9 @@ add_action('Flynt/afterRegisterComponents', function () {
                     'expert' => 'Expert',
                 ],
                 'default_value' => 'empty',
-                
+
             ],
-            
+
         ],
         'location' => [
             [
